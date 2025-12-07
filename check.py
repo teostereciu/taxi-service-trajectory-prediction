b5bcb0db10aa83fea27726d2fd4c70ba80b94370
teostereciu <teostereciu@gmail.com>
2025-12-07 01:27:12 +0000

    Engineered transitions

--- check.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-train = pd.read_parquet("data/transitions.parquet/train")
-test = pd.read_parquet("data/transitions.parquet/test")
-
-print(len(train), len(test))
-print(train.columns)
-print(train.head())
